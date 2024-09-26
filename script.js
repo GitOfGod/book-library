a094fb5b73014c0aa5777675e538fe4f88f19a3c
Favour <favouramike@gmail.com>
2024-09-26 11:03:48 +0100

    Refactor to use class instead of constructors
    - This will allow encapsulation

--- script.js
+++ script.js
@@ -1,76 +1,84 @@
 const myLibrary = [];
 
-function Book(title, author, pages, haveRead) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.haveRead = haveRead;
+class Book {
+    constructor(title, author, pages, haveRead) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.haveRead = haveRead;
+    }
+
+    toggleReadStatus() {
+        this.haveRead = this.haveRead === "Read" ? "Not read yet" : "Read";
+ 
+    }
 }
 
-Book.prototype.toggleReadStatus = function() {
-    this.haveRead = this.haveRead === "Read" ? "Not read yet" : "Read";
-}
+class Library {
+    constructor() {
+        this.myLibrary =[];
+    }
 
-function addBookToLibrary(title, author, pages, haveRead) {
-    const aBook = new Book(title, author, pages, haveRead);
-    myLibrary.push(aBook);
-    displayAllBooks();
-}
-
-function displayBook(book, index) {
-    const bookSection = document.querySelector('.book-section');
-        const div = document.createElement('div');
-        div.className = "book";
-
-        const title = document.createElement('h2');
-        const author = document.createElement('p');
-        const pages = document.createElement('p');
-        const haveRead = document.createElement('p');
-        const remove = document.createElement('button');
-        const toggleRead = document.createElement('button');
-
-        remove.className = 'remove';
-        remove.setAttribute('data-index', index);
-
-        title.textContent = `Title: ${book.title}`;
-        author.textContent = `Author: ${book.author}`;
-        pages.textContent =`Pages: ${book.pages}`;
-        haveRead.textContent = `Status: ${book.haveRead}`;
-        toggleRead.textContent = "Toggle Read Status";
-        remove.textContent = 'Remove';
-
-
-
-        div.appendChild(title);
-        div.appendChild(author);
-        div.appendChild(pages);
-        div.appendChild(haveRead);
-        div.appendChild(remove);
-        div.appendChild(toggleRead);
-        
-        bookSection.appendChild(div);
-
-        toggleRead.addEventListener('click', () => {
-            book.toggleReadStatus();
+    addBookToLibrary(title, author, pages, haveRead) {
+        const aBook = new Book(title, author, pages, haveRead);
+        myLibrary.push(aBook);
+        displayAllBooks();
+    }
+    displayBook(book, index) {
+        const bookSection = document.querySelector('.book-section');
+            const div = document.createElement('div');
+            div.className = "book";
+    
+            const title = document.createElement('h2');
+            const author = document.createElement('p');
+            const pages = document.createElement('p');
+            const haveRead = document.createElement('p');
+            const remove = document.createElement('button');
+            const toggleRead = document.createElement('button');
+    
+            remove.className = 'remove';
+            remove.setAttribute('data-index', index);
+    
+            title.textContent = `Title: ${book.title}`;
+            author.textContent = `Author: ${book.author}`;
+            pages.textContent =`Pages: ${book.pages}`;
             haveRead.textContent = `Status: ${book.haveRead}`;
-        })
+            toggleRead.textContent = "Toggle Read Status";
+            remove.textContent = 'Remove';
+    
+    
+    
+            div.appendChild(title);
+            div.appendChild(author);
+            div.appendChild(pages);
+            div.appendChild(haveRead);
+            div.appendChild(remove);
+            div.appendChild(toggleRead);
+            
+            bookSection.appendChild(div);
+    
+            toggleRead.addEventListener('click', () => {
+                book.toggleReadStatus();
+                haveRead.textContent = `Status: ${book.haveRead}`;
+            })
+    
+            remove.addEventListener('click', (event) => {
+                const bookIndex = event.target.getAttribute('data-index');
+                myLibrary.splice(bookIndex,1);
+                displayAllBooks();
+            });
+        }
+    
+        displayAllBooks() {
+            const bookSection = document.querySelector('.book-section');
+            bookSection.innerHTML = '';
+        
+            myLibrary.forEach((book, index) => {
+                displayBook(book, index);        
+            })
+        }
 
-        remove.addEventListener('click', (event) => {
-            const bookIndex = event.target.getAttribute('data-index');
-            myLibrary.splice(bookIndex,1);
-            displayAllBooks();
-        });
 
-
-}
-
-function displayAllBooks() {
-    const bookSection = document.querySelector('.book-section');
-    bookSection.innerHTML = '';
-
-    myLibrary.forEach((book, index) => {
-        displayBook(book, index);        
-    })
 }
 
 document.addEventListener('DOMContentLoaded', () => {
